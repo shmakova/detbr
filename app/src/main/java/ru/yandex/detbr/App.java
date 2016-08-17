5d99e3e453c13b017811dc37917ea49841673714
shmakova <shmakova-nastya@yandex.ru>
2016-08-17 16:43:45 +0300

    google analytics

--- app/src/main/java/ru/yandex/detbr/App.java
+++ app/src/main/java/ru/yandex/detbr/App.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 import io.fabric.sdk.android.Fabric;
 import ru.yandex.detbr.developer_settings.DevMetricsProxy;
 import ru.yandex.detbr.developer_settings.DeveloperSettingsModel;
@@ -12,6 +15,16 @@ import timber.log.Timber;
 
 public class App extends Application {
     private ApplicationComponent applicationComponent;
+    private Tracker tracker;
+
+    synchronized public Tracker getDefaultTracker() {
+        if (tracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            tracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return tracker;
+    }
 
     // Prevent need in a singleton (global) reference to the application object.
     @NonNull
