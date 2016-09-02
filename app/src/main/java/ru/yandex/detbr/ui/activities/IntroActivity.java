b948b5a8787e42e4825a2080002e6a8febb7bf32
shmakova <shmakova-nastya@yandex.ru>
2016-09-02 20:50:56 +0300

    New onboarding

--- app/src/main/java/ru/yandex/detbr/ui/activities/IntroActivity.java
+++ app/src/main/java/ru/yandex/detbr/ui/activities/IntroActivity.java
@@ -2,30 +2,34 @@ package ru.yandex.detbr.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-import com.github.paolorotolo.appintro.AppIntro;
+import com.viewpagerindicator.CirclePageIndicator;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.yandex.detbr.R;
-import ru.yandex.detbr.ui.fragments.IntroFragmentBuilder;
+import ru.yandex.detbr.ui.adapters.IntroFragmentStatePagerAdapter;
 import ru.yandex.detbr.ui.fragments.SchoolsFragment;
 
 /**
  * Created by shmakova on 26.08.16.
  */
 
-public class IntroActivity extends AppIntro implements SchoolsFragment.OnSchoolClickListener {
+public class IntroActivity extends AppCompatActivity implements SchoolsFragment.OnSchoolClickListener {
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+    @BindView(R.id.indicator)
+    CirclePageIndicator circlePageIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        showSkipButton(false);
-        setProgressButtonEnabled(false);
-        showStatusBar(true);
-        setFlowAnimation();
-        setSeparatorColor(ContextCompat.getColor(this, android.R.color.transparent));
-        addSlide(new IntroFragmentBuilder(R.layout.fragment_intro_first).build());
-        addSlide(new IntroFragmentBuilder(R.layout.fragment_intro_second).build());
-        addSlide(new SchoolsFragment());
+        setContentView(R.layout.activity_intro);
+        ButterKnife.bind(this);
+        viewPager.setAdapter(new IntroFragmentStatePagerAdapter(getSupportFragmentManager()));
+        circlePageIndicator.setViewPager(viewPager);
     }
 
     private void loadMainActivity() {
