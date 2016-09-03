b8397150e8f41120467065e520e7134eca0961b2
Denis Nabokov <user@annakoval-osx.local>
2016-09-03 20:56:40 +0300

    Fix dropDown width

--- app/src/main/java/ru/yandex/detbr/ui/fragments/SchoolsFragment.java
+++ app/src/main/java/ru/yandex/detbr/ui/fragments/SchoolsFragment.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -92,6 +93,10 @@ public class SchoolsFragment
         schoolsAdapter = new SchoolsAdapter(getActivity(),
                 R.layout.dropdown_item, R.id.item, new ArrayList<>());
         autoCompleteTextView.setAdapter(schoolsAdapter);
+
+        int width = autoCompleteTextView.getLayoutParams().width;
+        autoCompleteTextView.setDropDownWidth((int) (width * 1.5));
+        autoCompleteTextView.setDropDownHorizontalOffset(- width / 4);
 
         autoCompleteTextView.setOnItemClickListener((adapterView, v, i, l) -> {
             hideKeyboard();
