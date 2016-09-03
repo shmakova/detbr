d9fa22f2f4feb1980f784b4c17624ef281381af4
Denis Nabokov <user@annakoval-osx.local>
2016-09-03 21:08:27 +0300

    Fix clean build

--- app/src/main/java/ru/yandex/detbr/ui/fragments/SchoolsFragment.java
+++ app/src/main/java/ru/yandex/detbr/ui/fragments/SchoolsFragment.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -96,7 +95,7 @@ public class SchoolsFragment
 
         int width = autoCompleteTextView.getLayoutParams().width;
         autoCompleteTextView.setDropDownWidth((int) (width * 1.5));
-        autoCompleteTextView.setDropDownHorizontalOffset(- width / 4);
+        autoCompleteTextView.setDropDownHorizontalOffset(-width / 4);
 
         autoCompleteTextView.setOnItemClickListener((adapterView, v, i, l) -> {
             hideKeyboard();
