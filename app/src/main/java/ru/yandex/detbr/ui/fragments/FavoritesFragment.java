6088991c6bd6981594ab029ece7143d6f614d735
shmakova <shmakova-nastya@yandex.ru>
2016-09-06 21:42:42 +0300

    Return back LinearLayoutManager

--- app/src/main/java/ru/yandex/detbr/ui/fragments/FavoritesFragment.java
+++ app/src/main/java/ru/yandex/detbr/ui/fragments/FavoritesFragment.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -76,9 +76,9 @@ public class FavoritesFragment extends BaseLceFragment<FrameLayout, List<Card>, 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2,
-                GridLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(gridLayoutManager);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 
     protected String getErrorMessage(Throwable e, boolean pullToRefresh) {
