c05b7cc5d2834d3fc88da2d8f8eb38a1d1836321
shmakova <shmakova-nastya@yandex.ru>
2016-09-14 23:02:38 +0300

    Try to fix build error

--- app/src/main/java/ru/yandex/detbr/ui/animators/BackgroundAnimator.java
+++ app/src/main/java/ru/yandex/detbr/ui/animators/BackgroundAnimator.java
@@ -1,10 +1,6 @@
 package ru.yandex.detbr.ui.animators;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.view.View;
-import android.view.ViewAnimationUtils;
-import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by shmakova on 14.09.16.
@@ -14,21 +10,6 @@ public class BackgroundAnimator {
     public static void setCategoryCardsBackwardBackgroundColor(View backgroundView,
                                                                View previousBackgroundView,
                                                                int color, int x, int y) {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            backgroundView.setBackgroundColor(color);
-            int finalRadius = Math.max(backgroundView.getWidth(), backgroundView.getHeight());
-            Animator anim = ViewAnimationUtils.createCircularReveal(backgroundView, x, y, 0, finalRadius);
-            anim.setDuration(500);
-            anim.setInterpolator(new AccelerateInterpolator());
-            anim.start();
-            anim.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    previousBackgroundView.setBackgroundColor(color);
-                }
-            });
-        } else {
-            backgroundView.setBackgroundColor(color);
-        }
+
     }
 }
